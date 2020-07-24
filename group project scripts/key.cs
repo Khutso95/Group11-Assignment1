549cdcc8f53ba41d8cb097e36aa661e1bf34f482
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-07-24 13:29:13 +0200

    Create key.cs

---
+++ group project scripts/key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    //set key value to 1 for UI 
+    public int kval = 1;
+
+    //on trigger enter for player game object, only the player can pick the key up
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.gameObject.CompareTag("Player"))
+        {
+            keyscore.instance.changeKeynum(kval);
+        }
+    }
+}
