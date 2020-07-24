af9baed78104baf9230b7fcf1b19a7f783717bea
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-07-24 13:29:57 +0200

    Create platformmovement.cs

---
+++ platform movement/platformmovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformmovement : MonoBehaviour
+{
+    //all variables used
+    float directiononX;
+    public float speed = 3f;
+    bool moveR = true;
+    //max distance the object can move variables
+    public float Rmaxmove=4f;
+    public float Lmaxmove = -4f;
+
+
+
+    //use update to make platform move
+    void Update()
+    {
+        //will start to move left when rmaxmove is reached
+        if (transform.position.x > Rmaxmove)
+        {
+            moveR = false;
+        }
+         
+        //will move right when lmaxmove is reached, must be negative
+        if(transform.position.x<Lmaxmove)
+        {
+            moveR = true;
+        }
+
+        //this applies movement to the gameobject connected using transform.position
+        if(moveR)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
